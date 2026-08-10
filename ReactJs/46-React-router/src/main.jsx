c3f0a1a14f4f6401795226808376110c8b8329e5
Ravikishan Pandey <pandeyravi2143@gmail.com>
2026-08-10 13:47:19 +0530

    data fetching

--- ReactJs/46-React-router/src/main.jsx
+++ ReactJs/46-React-router/src/main.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <PostList /> },
+      { path: "/", element: <PostList />, loader: () => {} },
       { path: "/create-post", element: <CreatePost /> },
     ],
   },
